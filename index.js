3bc9c627f9517d8edf568958011f7d58c8656bf7
Aleksei Rogozin <alrogozin@gmail.com>
2022-12-01 13:45:11 +0300

    The first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const config = require('config');
+
+const dbConnection = config.get('DBConnection');
+
+const dbConn = {
+    iHost: '',
+    iUser: '',
+    iPort: '',
+}
+
+let host = '';
+
+if (config.has('DBConnection.host')) {
+   dbConn.iHost = config.get('DBConnection.host');
+} else {
+   throw new Error('error in config DBConnection.host');
+}
+
+console.log(dbConn.iHost);
